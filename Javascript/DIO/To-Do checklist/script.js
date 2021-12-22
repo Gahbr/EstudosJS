63d1c26a000cc8b9e08e7fd2ab285cabb82fc32e
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2021-12-22 19:19:07 -0300

    projeto de checklist

---
+++ Javascript/DIO/To-Do checklist/script.js
@@ -0,0 +1,30 @@
+function add() {
+ 
+    var input = document.getElementById('texto').value;
+    var lista = document.getElementById("lista");
+
+    // criando input
+    var checkbox = document.createElement('input');
+
+    // definindo os atributos da checkbox
+    checkbox.type = "checkbox";
+    checkbox.value = "value";
+    checkbox.id = "id";
+
+    // criando label para texto da checkbox
+    var label = document.createElement('label');
+
+    // colocando atributos para a tab label
+    label.htmlFor = "id";
+
+    // dando append no texto criado para a tag label
+    label.innerHTML += input + "</br>"
+
+ // dando append na checkbox e label para a div
+        lista.appendChild(checkbox);
+        lista.appendChild(label);
+        }
+
+function limparTela(){
+    document.getElementById('label').innerHTML = "";
+}
