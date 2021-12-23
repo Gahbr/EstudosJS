10f3a6ce263bc8b5596b85f45cbd3065b0532a46
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2021-12-23 11:29:15 -0300

    desafio de operadores e sintaxe

--- Javascript/DIO/Sintaxe e operadores/script.js
+++ Javascript/DIO/Sintaxe e operadores/script.js
@@ -5,21 +5,20 @@ var res = document.getElementById('resultados');
 function calcular() {
     var soma = parseInt(num1) + parseInt(num2);
 
-if (num1 == num2 ) {
-    res.innerHTML += "Os números 1 e 2 são iguais. ";
-} else {
-    res.innerHTML+= "Os números 1 e 2 não são iguais. ";
-}
+    if (num1 == num2) {
+        res.innerHTML += "Os números 1 e 2 são iguais. ";
+    } else {
+        res.innerHTML += "Os números 1 e 2 não são iguais. ";
+    }
 
-res.innerHTML += "Sua soma é " + soma + ", ";
-(soma < 10 ) ? res.innerHTML += "que é menor que 10 " : res.innerHTML += "que é maior que 10 ";
-(soma < 20 ) ? res.innerHTML += "e menor que 20. " : res.innerHTML += "e maior que 20. ";
+    res.innerHTML += "Sua soma é " + soma + ", ";
+    (soma < 10) ? res.innerHTML += "que é menor que 10 " : res.innerHTML += "que é maior que 10 ";
+    (soma < 20) ? res.innerHTML += "e menor que 20. " : res.innerHTML += "e maior que 20. ";
 
-//sumir com o botao
-document.getElementById('botao').classList.add('hide');
+    //sumir com o botao
+    document.getElementById('botao').classList.add('hide');
 
 }
 
 /* <!-- Feito por Gabriel Ribeiro -- Github: github.com/Gahbr --> */
 
-    
