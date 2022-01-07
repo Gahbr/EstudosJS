509a153e1d37bab40d379d55b0453c6a456381be
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-01-07 18:19:25 -0300

    react

---
+++ Javascript/DIO/Introducao ao React/b7web/src/App.js
@@ -0,0 +1,19 @@
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+
+const App= () =>{
+    let name ="Parakeet";
+    let age = 90;
+
+    return (
+        <div>
+            <Header/>
+            ...
+            <Footer/>
+            
+        </div>
+    );
+}
+
+
+export default App;
---
+++ Javascript/DIO/Introducao ao React/b7web/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () =>{
+    return (
+        <header>
+            <h1>Algum título interessante.</h1>
+        </header>
+    );
+}
