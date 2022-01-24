d6d702498ccdc0bbe8845f7e463245d2bb89fb9d
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-01-24 15:17:34 -0300

    utilizando um componente com estado

--- Javascript/DIO/React States e Effects/aula-hooks/src/Components/App/App.js
+++ Javascript/DIO/React States e Effects/aula-hooks/src/Components/App/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from '../Counter/Counter';
+import SmartCounter from '../SmartCounter/SmartCounter';
 
 function App() {
   return (
     <>
     <h1>Hey</h1>
-    <Counter/>
-    
+    <SmartCounter/>
+
     </>
   );
 }
