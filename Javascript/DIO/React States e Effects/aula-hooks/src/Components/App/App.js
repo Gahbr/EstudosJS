1396ad7cbd8e593e2528b56bcaf86cd6470d70b3
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-01-24 17:17:39 -0300

    criando um componente pratico com estado

--- Javascript/DIO/React States e Effects/aula-hooks/src/Components/App/App.js
+++ Javascript/DIO/React States e Effects/aula-hooks/src/Components/App/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from '../Counter/Counter';
 import SmartCounter from '../SmartCounter/SmartCounter';
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
 
 function App() {
   return (
     <>
     <h1>Hey</h1>
-    <SmartCounter/>
+    <IfoodCounter/>
 
     </>
   );
