9afac28e95cacc1066b4fed5b834db9ae4474b86
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-01-24 13:39:24 -0300

    criei um componente sem utilizar estados

--- Javascript/DIO/React States e Effects/aula-hooks/src/Components/App/App.js
+++ Javascript/DIO/React States e Effects/aula-hooks/src/Components/App/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from '../Counter/Counter';
 
 function App() {
   return (
+    <>
     <h1>Hey</h1>
+    <Counter/>
+    
+    </>
   );
 }
 
