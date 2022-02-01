140fcb6e4aff7ca3807630bd517eee776948edc6
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-02-01 16:57:05 -0300

    criando o servidor com express

--- Javascript/DIO/node api rest/src/index.ts
+++ Javascript/DIO/node api rest/src/index.ts
@@ -1,1 +1,11 @@
-console.log("parakeet");
+import express, {Request,Response,NextFunction} from 'express'; //gerenciador de rotas http
+
+const app = express();
+app.get('/status', (req: Request, res: Response, next:NextFunction)=> {
+    res.status(200).send({foo:'bar'})
+})
+
+app.listen (3000, ()=>{
+    console.log("Aplicacao executando na porta 3000.");
+    
+})
