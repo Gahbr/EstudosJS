23570b92dbff7e3b70d2e3996735b32506f24b9b
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-03-28 12:43:37 -0300

    finalizando api frases

--- Javascript/B7WEB/Node/Modulo 4 - Node API/src/controller/apiController.ts
+++ Javascript/B7WEB/Node/Modulo 4 - Node API/src/controller/apiController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 import * as ApiController from './apiController';
 import {Phrase} from '../models/Phrase'
+import { Sequelize } from 'sequelize';
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong:true});
@@ -21,4 +22,56 @@ export const createPhrase = async(req: Request, res: Response) => {
    let newPhrase =  await Phrase.create({author, txt});
    res.status(201);
     res.json({id: newPhrase.id, author , txt})
+}
+
+export const listPhrases = async(req: Request, res: Response) => {
+    let list = await Phrase.findAll();
+    res.json({list});
+}
+
+export const getPhrase = async(req: Request, res: Response) => {
+    let {id} = req.params;
+    let phrase = await Phrase.findByPk(id);
+
+    if(phrase){
+        res.json(phrase);
+    }else{
+        res.json({error:"Frase não encontrada!"})
+    }
+    
+}
+
+export const updatePhrase = async(req: Request, res: Response) => {
+    let {id} = req.params;
+    let {author, txt} = req.body;
+    let phrase = await Phrase.findByPk(id);
+
+    if(phrase){
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+        res.json({ phrase });
+    }else{
+        res.json({error:"Frase não encontrada!"})
+    }
+}
+
+export const deletePhrase = async(req: Request, res: Response) => {
+    let {id} = req.params;
+    let phrase = await Phrase.findByPk(id);
+
+    if(phrase){
+        await phrase.destroy();
+        res.json({message:"Frase excluida com sucesso!"})
+    }else{
+        res.json({error:"Frase não encontrada!"})
+    }
+};
+
+export const randomPhrase = async(req: Request, res: Response) => {
+    let list = await Phrase.findAll();
+    let nRand = Math.floor(Math.random()*list.length);
+    res.json(list[nRand]);
+   
+    
 }
--- Javascript/B7WEB/Node/Modulo 4 - Node API/src/routes/api.ts
+++ Javascript/B7WEB/Node/Modulo 4 - Node API/src/routes/api.ts
@@ -8,6 +8,13 @@ router.get('/ping', ApiController.ping);
 router.get('/random',ApiController.random);
 router.get('/nome/:nome', ApiController.nome);
 
+router.get('/frases',ApiController.listPhrases);
+router.get('/frases/aleatoria', ApiController.randomPhrase);
+router.get('/frases/:id',ApiController.getPhrase);
 router.post('/frases', ApiController.createPhrase);
+router.put('/frases/:id', ApiController.updatePhrase);
+router.delete('/frases/:id', ApiController.deletePhrase);
+
+
 
 export default router;
