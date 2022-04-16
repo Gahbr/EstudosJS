fbb6ae1efcd8b01c22f3eeaf61682efcf16d6648
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-04-16 13:48:48 -0300

    crud basico em angular

---
+++ Javascript/Capgemini Start/Angular/exemplo04/src/app/cursos/cursos.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from './Curso';
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent implements OnInit {
+
+  //atributos
+  public vetorCursos!: Curso[];
+  public curso!: Curso;
+  public id!: number;
+
+  //construtor
+  constructor(private servico:CursosService) { }
+
+  ngOnInit()  {
+    this.id = -1;
+    this.curso =  new Curso();
+    this.vetorCursos = this.servico.listar();
+  }
+
+  //cadastrar
+  cadastrar(){
+   this.servico.cadastrar(this.curso);
+   this.curso = new Curso();
+  }
+
+  //excluir
+  excluir(id:number){
+    this.servico.excluir(id);
+    this.id = -1;
+}
+
+//Editar
+editar(id:number){
+  this.id = id;
+
+  this.curso = new Curso(
+    this.vetorCursos[id].nomeCurso,
+    this.vetorCursos[id].valorCurso,
+    this.vetorCursos[id].areaCurso)
+}
+
+//atualizar
+atualizar(){
+  this.servico.alterar(this.id, this.curso);
+  this.curso= new Curso();
+  this.id = -1;
+
+}
+}
