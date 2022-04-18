952c991df6d0f10751079e0e4eea4a2c525dd6a2
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-04-18 16:07:30 -0300

    aulas angular

---
+++ Javascript/Capgemini Start/Angular/exemplo05/src/app/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  //variavel de acesso
+  acesso:boolean = true;
+
+  //metodo
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.acesso;
+  }
+  
+}
