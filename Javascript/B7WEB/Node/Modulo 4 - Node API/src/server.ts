4e1501c5dacdaff81d48567b749e776cd1286e52
Gabriel Ribeiro <80289718+Gahbr@users.noreply.github.com>
2022-05-24 12:53:08 -0300

    tratamento de erros

--- Javascript/B7WEB/Node/Modulo 4 - Node API/src/server.ts
+++ Javascript/B7WEB/Node/Modulo 4 - Node API/src/server.ts
@@ -1,8 +1,9 @@
-import express,{Request,Response} from 'express';
+import express,{Request,Response, ErrorRequestHandler} from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
 import cors from 'cors';
+import {MulterError} from 'multer';
 
 dotenv.config();
 const server = express();
@@ -17,7 +18,17 @@ server.use((req:Request, res:Response)=>{
     res.json({error: 'Endpoint not found'});
 });
 
-
+const errorHandler : ErrorRequestHandler = (err, req, res, next) =>{
+    res.status(400); // bad request
+    if ( err instanceof MulterError ){
+        res.json({error: err.code})
+    } else{
+        console.log(err);
+        res.json({error:'Ocorreu algum erro'})
+        
+    }
+}
+server.use(errorHandler);
 
 // start server
 server.listen(process.env.PORT);
