6254eec9d2eae0a813f2bea6c95cbe19f073c987
Gabriel Ribeiro <gabriell.ribeiro@yahoo.com>
2022-07-09 22:04:43 -0300

    react stylescomponent

---
+++ Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/styled-components/src/App.tsx
@@ -0,0 +1,21 @@
+
+// import {Botao}from "./components/Botao";
+// import { Container } from "./components/Container";
+
+// import {Container, Botao} from './AppStyles'
+
+import * as C from './AppStyles'
+
+
+
+
+const App = ()=> {
+  return (
+    <C.Container> texto do componente
+      <C.Botao>Clique aqui</C.Botao>
+    </C.Container>
+    
+  )
+}
+
+export default App;
