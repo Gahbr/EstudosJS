1e1a60cf8c27b19f589372a3694bb6d143aa5ffb
Gabriel Ribeiro <gabriell.ribeiro@yahoo.com>
2022-08-02 18:23:16 -0300

    tailwind

--- Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/tailwind/src/App.tsx
+++ Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/tailwind/src/App.tsx
@@ -2,11 +2,14 @@
 const App = ()=> {
   
   return (
-    <div className='bg-black'>
-     asdasd
-     
+    <div className='bg-blue-900 text-white p-4 text-2x1 font-bold flex flex-col mt-3 text-center md:flex-row md:text-green-800 lg:text-black md:justify-between '>
+     <input className= "outline-none border border-transparent bg-white p-3 rounded focus:ring-2 focus:ring-blue-500" 
+     type="text"
+      placeholder="digite alguma coisa"
+      />
+     <button className="bg-green-300 text-black p-2 rounded-2xl mt-3 md:ml-3 md:mt-0 hover:bg-amber-600">Lorem ipsum dolor sit amet.</button>
     </div>
   )
 }
 
-export default App
+export default App;
