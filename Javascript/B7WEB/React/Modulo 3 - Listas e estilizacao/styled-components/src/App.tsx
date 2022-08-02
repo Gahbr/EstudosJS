cce3d6096a1615dfb80b6babda19698301643925
Gabriel Ribeiro <gabriell.ribeiro@yahoo.com>
2022-08-02 14:54:01 -0300

    aulas styledcomponents

--- Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/styled-components/src/App.tsx
+++ Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/styled-components/src/App.tsx
@@ -1,18 +1,18 @@
-
 // import {Botao}from "./components/Botao";
 // import { Container } from "./components/Container";
-
 // import {Container, Botao} from './AppStyles'
 
 import * as C from './AppStyles'
 
 
 
-
 const App = ()=> {
   return (
-    <C.Container> texto do componente
-      <C.Botao>Clique aqui</C.Botao>
+    <C.Container bgColor = "beige"> 
+    <span>texto do componente </span>
+    <a href="" className='link'>Link qualquer |</a>
+      <C.Botao bg="red">Botao grande</C.Botao>
+      <C.Botao bg="darkgreen" small >Botao pequeno</C.Botao> 
     </C.Container>
     
   )
--- Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/styled-components/src/AppStyles.ts
+++ Javascript/B7WEB/React/Modulo 3 - Listas e estilizacao/styled-components/src/AppStyles.ts
@@ -1,10 +1,44 @@
 import styled from "styled-components";
 
-export const Botao = styled.button`
-font-size:40px;
+type BotaoProps = {
+    small ?: boolean;
+    bg : string
+}
+
+type ContainerProps = {
+    bgColor: string;
+}
+
+export const Botao = styled.button<BotaoProps>`
+  font-size:${props => props.small ? '15px' : '30px'};
+  background-color: ${props => props.bg};
+
 `;
 
-export const Container = styled.div`
-  background-color: blue;
-  color:white;
+export const Container = styled.div<ContainerProps>`
+  max-width: 600px;
+  margin: auto;
+  background-color: ${props => props.bgColor};
+  color: black;
+  padding: 20px;
+  display: flex;
+
+  span{ font-weight: bold }
+
+  .link {
+    color:#ccc;
+
+    &:hover{
+    color:pink}
+  }
+  
+  @media (max-width:500px){
+    background-color: darkgreen;
+    flex-direction: column;
+
+      span{
+        color: blue;
+      }
+  }
+
 `;
