c279987c073252f90c2751179c0c4edf433b2449
Gabriel Ribeiro <gabriell.ribeiro@yahoo.com>
2022-08-10 01:41:03 -0300

    iniciando projeto

---
+++ Javascript/DIO/Api-node-orm/index.ts
@@ -0,0 +1,14 @@
+import  express  from "express";
+
+const server = express();
+
+server.listen(5000, ()=>{ console.log("Servidor on na porta 5000")})
+
+//API RESTFUL
+//Criar um usuario
+
+//Editar um usuario
+
+//Selecionar um usuario
+
+//Deletar um usuario
