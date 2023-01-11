c6abe077a1fa86b0aa99707f25bbda4472213efe
Gabriel Ribeiro <gabriell.ribeiro@yahoo.com>
2023-01-11 16:36:18 -0300

    iniciando testes

---
+++ Javascript/B7WEB/Node/Modulo 9 - Testes e TDD/src/libs/Math.test.ts
@@ -0,0 +1,27 @@
+import {Math} from './Math'
+
+describe('Testando a biblioteca Math', ()=>{
+
+    it('Should sum two numbers correctly', ()=>{
+        const response = Math.sum(5,10)
+        expect(response).toBe(15);
+    })
+    
+    it('Should subtract two numbers correctly', ()=>{
+        const response = Math.sub(4,2)
+        expect(response).toBe(2);
+    })
+    
+    it('Should multiply two numbers correctly', ()=>{
+        const response = Math.mult(3,5);
+        expect(response).toBe(15);
+    })
+    
+    it('Should divide two numbers correctly', ()=>{
+        const response = Math.div(15,5)
+        expect(response).toBe(3);
+    
+        const response2 = Math.div(3,0)
+        expect(response2).toBe(false);
+    })
+})
