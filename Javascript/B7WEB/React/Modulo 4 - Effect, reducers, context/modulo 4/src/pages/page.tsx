32447581ed362025d4baeb739885474126159d0a
Grimnir-WSL <gabriell.ribeiro@yahoo.com>
2023-09-08 22:03:49 -0300

    reducer add item

--- Javascript/B7WEB/React/Modulo 4 - Effect, reducers, context/modulo 4/src/pages/page.tsx
+++ Javascript/B7WEB/React/Modulo 4 - Effect, reducers, context/modulo 4/src/pages/page.tsx
@@ -1,21 +1,56 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { listReducer } from "../reducers/listReducer";
 
 const Page = () => {
+  
   const [list, dispatch] = useReducer(listReducer, []);
-  console.log(list);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    console.log(list);
+  }, [list])
+  
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    setInput(e.target.value)
+  }
+
+  
 const handleAddClick = () => {
   dispatch({
     type:'add',
     payload: {
-      text:'Novo item'
+      text: input
     }
   })
+
+  setInput("")
+};
+const handleEditClick = (idValue: number) => {
+  dispatch({
+    type:'editText',
+    payload: {
+     id: idValue,
+      newText: input 
+    }
+  })
+
+ return (
+  <div>
+    yae
+  </div>
+ )
 };
 
 return (
   <div>
-    <button onClick={handleAddClick}>Adicionar</button>
+    <h1>TO-DO</h1>
+    <ul>
+      {list.map((item, index) =>(
+        <li key={index}>{item.id} | {item.text} | <input type="checkbox"></input> <button style={{margin:'10px'}} onClick={()=>handleEditClick(item.id)}>Editar</button></li>
+        
+      ))}
+    </ul>
+     <input type="text" name="add" id="" onChange={handleOnChange} value={input || "" } /><button onClick={handleAddClick}>Adicionar</button>
   </div>
 );
 }
